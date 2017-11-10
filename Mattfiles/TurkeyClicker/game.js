6e77d7144109f4623b6603566da2ffb07df9448d
matt kaufman <aggrieved@yopmail.com>
2017-11-10 15:30:21 +0000

    cleaning up

--- Mattfiles/TurkeyClicker/game.js
+++
@@ -1,9 +0,0 @@
-let score = 0;
-let cookieElement = document.getElementById('cookie');
-let scoreElement = document.getElementById('score');
-
-
-cookieElement.addEventListener('click', function () {
-    score++;
-    scoreElement.textContent = score + " coins";
-});
